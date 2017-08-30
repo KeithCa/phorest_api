350a0122a3ddd70abb1db1c028f6187fc13be8fa
Unknown <c12406518@mydit.ie>
2017-08-30 12:59:01 +0100

    search result displaying

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -9,6 +9,11 @@ import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import org.json.JSONObject;
+import org.json.JSONArray;
+import javafx.scene.control.Label;
 
 
 public class Controller {
@@ -22,25 +27,28 @@ public class Controller {
     String password = "VMlRo/eh+Xd8M~l";
     //String phone_no = "888888855599222";
 
-    public static void main(String[] args) throws Exception {
-        Controller client = new Controller();
-        client.getClientsbyPhone("0868694227");
+    @FXML private Label info;
+
+
+    public static void main(String[] args){
     }
 
-    public void getClientsbyPhone(String phone_no) throws Exception {
+    public JSONObject getClientsbyPhone(String phone_no) throws Exception {
         String base_url = "https://api-gateway-dev.phorest.com/third-party-api-server/api/business/eTC3QY5W3p_HmGHezKfxJw/client";
         HttpResponse<JsonNode> response = Unirest.get(base_url).basicAuth(username, password).
                 queryString("phone", phone_no).
                 asJson();
-        System.out.println(response.getBody().getObject().toString(2));
+        JSONObject responses = response.getBody().getObject().getJSONObject("_embedded");
+        return responses;
     }
 
-    public void getClientsbyEmail(String email) throws Exception {
+    public JSONObject getClientsbyEmail(String email) throws Exception {
         String base_url = "https://api-gateway-dev.phorest.com/third-party-api-server/api/business/eTC3QY5W3p_HmGHezKfxJw/client";
         HttpResponse<JsonNode> response = Unirest.get(base_url).basicAuth(username, password).
                 queryString("email", email).
                 asJson();
-        System.out.println(response.getBody().getObject().toString(2));
+        JSONObject responses = response.getBody().getObject().getJSONObject("_embedded");
+        return responses;
     }
 
     @FXML
@@ -48,19 +56,31 @@ public class Controller {
         Stage stage;
         Parent root;
         Controller client = new Controller();
-
+        JSONObject response = new JSONObject();
 
         if(event.getSource() == searchBtn) {
             String search = searchField.getText();
             String choice = choices.getSelectionModel().getSelectedItem().toString();
             if(choice.equals("Phone Number")){
                 System.out.println(search);
-                client.getClientsbyPhone(search);
+                response = client.getClientsbyPhone(search);
             }
             else {
                 client.getClientsbyEmail(search);
+                response = client.getClientsbyPhone(search);
             }
+            JSONArray clients = response.getJSONArray("clients");
 
+            String name, sname, phone, email, add;
+
+            JSONObject client1 = clients.getJSONObject(0);
+            name = client1.getString("firstName");
+            sname = client1.getString("lastName");
+            phone = client1.getString("mobile");
+            email = client1.getString("email");
+            add = client1.getString("clientSince");
+
+            info.setText("Name: "+name+" "+sname+"\n\nPhone: "+phone+"\n\nEmail: "+email+"\n\nClient Since: "+add);
         }
 
     }
